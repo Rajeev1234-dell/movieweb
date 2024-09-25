9b4fabc211dd2828354391d3b2505435495231a1
Rajeev <pmdbrajeev1@gmail.com>
2024-09-25 11:17:20 +0530

    setup-completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1400px",
+          "3xl": "1594px",
+        },
+      },
+      fontFamily: {
+        manrope: "Manrope",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#141414",
+        gray: "#999999",
       },
     },
   },
